1e1da072f7b4034f73c77e6dcb76bd725571cb12
Ernest Chechelski <ernichechelski@gmail.com>
2020-01-31 10:13:00 +0100

    Initial Commit

---
+++ SourceryUsaCases/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SourceryUsaCases
+//
+//  Created by Ernest Chechelski on 31/01/2020.
+//  Copyright © 2020 Ernest Chechelski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
