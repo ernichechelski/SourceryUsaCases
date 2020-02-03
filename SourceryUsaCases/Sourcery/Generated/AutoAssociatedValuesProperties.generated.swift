ae3b8a82006dee726beab40fe3fa1de2670581d5
Ernest Chechelski <ernichechelski@gmail.com>
2020-02-03 12:13:02 +0100

    Fixed template for associated values

--- SourceryUsaCases/Sourcery/Generated/AutoAssociatedValuesProperties.generated.swift
+++ SourceryUsaCases/Sourcery/Generated/AutoAssociatedValuesProperties.generated.swift
@@ -7,7 +7,7 @@ internal extension DrivetrainType {
     var type: AWDType? {
         switch self {
              case .AWD(let type):
-                return type
+                return type 
              case .FWD:
                 return nil
              case .RWD:
@@ -15,63 +15,25 @@ internal extension DrivetrainType {
         }
     }
 
-        var AWDAssociatedValues: (type: AWDType)? {
-            switch self {
-                 case .AWD(let type):
-                    return (type)
-                  default: return nil
-            }
-        }
 }
 internal extension Vehicle {
 
-    var description: VehicleData? {
-        switch self {
-             case .car(let description,let engineType,let drivetrain):
-                return description
-             case .rocketlet engines):
-                return description
-        }
-    }
-    var engineType: EngineType? {
-        switch self {
-             case .car(let description,let engineType,let drivetrain):
-                return engineType
-             case .rocketlet engines):
-                return engineType
-        }
-    }
-    var drivetrain: DrivetrainType? {
-        switch self {
-             case .car(let description,let engineType,let drivetrain):
-                return drivetrain
-             case .rocketlet engines):
-                return drivetrain
-        }
-    }
     var engines: Int? {
         switch self {
              case .car(let description,let engineType,let drivetrain):
-                return engines
-             case .rocketlet engines):
-                return engines
+                return nil 
+             case .rocket(let engines):
+                return engines 
         }
     }
 
-        var carAssociatedValues: (description: VehicleData, engineType: EngineType, drivetrain: DrivetrainType)? {
-            switch self {
-                 case .car(let description,let engineType,let drivetrain):
-                    return (description,engineType,drivetrain)
-                  default: return nil
-            }
+    var carAssociatedValues: (description: VehicleData, engineType: EngineType, drivetrain: DrivetrainType)? {
+        switch self {
+            case .car(let description,let engineType,let drivetrain):
+                return (description,engineType,drivetrain)
+            default: return nil
         }
-        var rocketAssociatedValues: (engines: Int)? {
-            switch self {
-                 case .rocket(let engines):
-                    return (engines)
-                  default: return nil
-            }
-        }
+    }
 }
 
 
